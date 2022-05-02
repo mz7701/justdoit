3f3d2164ea647cd5f3c760af175ad32b3a2c89e8
mz7701 <korean2071@naver.com>
2022-05-02 18:23:13 +0900

    yes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
+const sequelize = require('./models').sequelize;
+sequelize.sync();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const {
+    Teacher,
+    Sequelize: { Op }
+  } = require('./models');
+sequelize.query('SET NAMES utf8;');
+
+app.post('/add/data', (req, res) => {
+    console.log(req.body)
+
+      Teacher.create({
+          name : req.body.data
+      })
+      .then( result => {
+          res.send(result)
+      })
+      .catch( err => {
+          console.log(err)
+          throw err;
+      })
+}) 
+
 const PORT = process.env.PORT || 4000;
-
-app.get('/', (req, res) => {
-    res.send(`Response Complate`);
-})
-
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
-  })
+})
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name : '',
+    }
+  }
+
+  _addData = async(e) => {
+    const { name } = this.state;
+    e.preventDefault();
+    
+    const res = await axios('/add/data', {
+      method : 'POST',
+      data : { 'data' : name },
+      headers: new Headers()
+    })
+
+    if(res.data) {
+      alert('데이터를 추가했습니다.');
+      return window.location.reload();
+    }
+  }
+
+  _nameUpdate(e) {
+    this.setState({ name : e.target.value })
+  }
+
+  render() {
+    return(
+      <div className='App'>
+        <h3> Welcome to <u> seungmyeong </u> Blog! </h3>
+        <h5> https://seungmyeong.blog.me/ </h5>
+
+        <br />
+        <form method='POST' onSubmit={this._addData}>
+          <input type='text' maxLength='10' onChange={(e) => this._nameUpdate(e)}/>
+          <input type='submit' value='Add' />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
